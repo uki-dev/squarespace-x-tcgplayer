4636007a1ea130c2b40be7f2d25c5d1ad5d24eb0
uki <hello@uki.dev>
2025-04-25 16:01:32 +1000

    🃏✨ Add support for updating both `Near Mint` and `Near Mint Foil` variant prices

--- update-prices.ts
+++ update-prices.ts
@@ -11,6 +11,7 @@ if (!SQUARESPACE_API_KEY) {
 }
 
 namespace Squarespace {
+  // TODO: use `zod`?
   export interface Variant {
     id: string;
     sku: string;
@@ -19,18 +20,56 @@ namespace Squarespace {
         currency: string;
         value: string;
       };
+      salePrice?: {
+        currency: string;
+        value: string;
+      };
+      onSale?: boolean;
+    };
+    stock?: {
+      quantity: number;
+      unlimited: boolean;
+    };
+    attributes?: Record<string, string>;
+    image?: {
+      id: string;
+      title: string;
+      url: string;
+      originalSize: {
+        width: number;
+        height: number;
+      };
+      availableFormats: string[];
     };
   }
 
   export interface Product {
     id: string;
+    type: 'PHYSICAL' | 'DIGITAL';
     name: string;
-    variants: Variant[];
+    description?: string;
+    url: string;
+    variants?: Variant[];
+    pricing?: {
+      basePrice: {
+        currency: string;
+        value: string;
+      };
+      salePrice?: {
+        currency: string;
+        value: string;
+      };
+      onSale?: boolean;
+    };
   }
-
 
   export interface ProductResponse {
     products: Product[];
+    pagination: {
+      hasNextPage: boolean;
+      nextPageCursor: string;
+      nextPageUrl: string;
+    };
   }
 
   const BASE_URL = 'https://api.squarespace.com/1.0/commerce/products';
@@ -46,29 +85,33 @@ namespace Squarespace {
     return data.products;
   }
 
-  export async function updateProductPrice(product: Product, newPrice: number): Promise<void> {
-    if (product.variants.length === 0) {
-      console.warn(`Product [${product.name}] has no variants to update.`);
-      return;
-    }
+  export async function updateProductVariant(
+    productId: string,
+    variantId: string,
+    price: number,
+  ): Promise<void> {
+    const body = {
+      pricing: {
+        basePrice: {
+          currency: 'AUD',
+          value: price.toFixed(2)
+        }
+      }
+    };
 
-    const variant = product.variants[0];
-
-    await fetch(`${BASE_URL}/${product.id}`, {
-      method: 'PUT',
+    const response = await fetch(`${BASE_URL}/${productId}/variants/${variantId}`, {
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${SQUARESPACE_API_KEY}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        variants: [
-          {
-            id: variant.id,
-            price: newPrice
-          }
-        ]
-      })
+      body: JSON.stringify(body)
     });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error(`❌ Failed to update variant ${variantId}: ${errorText}`);
+    }
   }
 
   export async function backupProducts(products: Product[]): Promise<void> {
@@ -80,7 +123,7 @@ namespace Squarespace {
 }
 
 namespace TCGPlayer {
-  export async function scrapeCardPrice(cardName: string): Promise<number | null> {
+  export async function scrapeCardPrices(cardName: string) {
     const browser = await puppeteer.launch({
       headless: true,
       args: [
@@ -89,22 +132,39 @@ namespace TCGPlayer {
     });
     const page = await browser.newPage();
     try {
-      const searchUrl = `https://www.tcgplayer.com/search/all/products?q=${encodeURIComponent(cardName)}`;
-      await page.goto(searchUrl, { waitUntil: 'domcontentloaded' });
-      await page.waitForSelector('.product-card');
-      const price = await page.evaluate(() => {
-        const firstProductCard = document.querySelector('.product-card');
-        if (firstProductCard) {
-          const priceElement = firstProductCard.querySelector('.product-card__market-price .product-card__market-price--value');
-          if (priceElement) {
-            const priceText = priceElement.textContent?.trim();
-            const parsedPrice = parseFloat(priceText?.replace(/[^\d.-]/g, '') ?? '');
-            return isNaN(parsedPrice) ? null : parsedPrice;
-          }
-        }
+      await page.goto(`https://www.tcgplayer.com/search/all/products?q=${encodeURIComponent(cardName)}`, { waitUntil: 'domcontentloaded' });
+
+      await page.waitForSelector('.product-card', { visible: true });
+
+      const card = await page.evaluate((cardName) => {
+        const cards = Array.from(document.querySelectorAll('.product-card'));
+        const matchedCard = cards.find(card => {
+          const title = card.querySelector('.product-card__title')?.textContent?.trim() || '';
+          return cardName.toLowerCase().includes(title.toLowerCase());
+        });
+        return matchedCard ? matchedCard.querySelector('a')?.href : null;
+      }, cardName);
+
+
+      if (!card) {
         return null;
+      }
+
+      await page.goto(card, { waitUntil: 'domcontentloaded' });
+
+      await page.waitForFunction(() => {
+        const prices = Array.from(document.querySelectorAll('.near-mint-table__price'));
+        return prices.length >= 2 && prices.every(el => (el.textContent || '').trim() !== '');
       });
-      return price;
+
+      const { normalPrice, foilPrice } = await page.evaluate(() => {
+        const prices = Array.from(document.querySelectorAll('.near-mint-table__price'));
+        const normalPrice = prices[0]?.textContent?.replace(/[^\d.-]/g, '') || null;
+        const foilPrice = prices[1]?.textContent?.replace(/[^\d.-]/g, '') || null;
+        return { normalPrice, foilPrice };
+      });
+
+      return { normalPrice, foilPrice };
     } finally {
       await browser.close();
     }
@@ -120,18 +180,41 @@ namespace TCGPlayer {
     for (const [index, product] of products.entries()) {
       console.group(`🔄 (${index + 1}/${products.length}) ${product.name}`);
 
-      const oldPrice = product.variants.length > 0 && product.variants[0].pricing
-        ? parseFloat(product.variants[0].pricing.basePrice.value)
-        : null;
-      const newPrice = await TCGPlayer.scrapeCardPrice(product.name);
-
-      if (newPrice == null) {
-        console.warn("⚠️ Failed to query price")
+      const prices = await TCGPlayer.scrapeCardPrices(product.name);
+      if (!prices) {
+        console.warn(`⚠️ No prices found for ${product.name}`);
+        console.groupEnd();
+        continue;
       }
 
-      if (newPrice !== null && oldPrice !== null && oldPrice !== newPrice) {
-        console.log(`✅ Price updated from $${oldPrice.toFixed(2)} to $${newPrice.toFixed(2)}`);
-        await Squarespace.updateProductPrice(product, newPrice);
+      const nearMintVariant = product.variants?.find(v => v.attributes?.["Condition"] === "Near Mint");
+
+      if (nearMintVariant && prices.normalPrice) {
+        console.group("🃏 Near Mint");
+        const oldPrice = parseFloat(nearMintVariant.pricing.basePrice.value);
+        const newPrice = parseFloat(prices.normalPrice);
+
+        if (oldPrice !== newPrice) {
+          await Squarespace.updateProductVariant(product.id, nearMintVariant.id, newPrice);
+          console.log(`✅ Update in price from $${oldPrice} → $${newPrice}`);
+        } else {
+          console.log(`⏸️ No change in price from $${oldPrice}`);
+        }
+        console.groupEnd();
+      }
+
+      const nearMintFoilVariant = product.variants?.find(v => v.attributes?.["Condition"] === "Near Mint Foil");
+      if (nearMintFoilVariant && prices.normalPrice) {
+        console.group("🃏✨ Near Mint Foil");
+        const oldPrice = parseFloat(nearMintFoilVariant.pricing.basePrice.value);
+        const newPrice = parseFloat(prices.normalPrice);
+        if (oldPrice !== newPrice) {
+          await Squarespace.updateProductVariant(product.id, nearMintFoilVariant.id, newPrice);
+          console.log(`✅ Updated price from $${oldPrice} → $${newPrice}`);
+        } else {
+          console.log(`⏸️ No change in price from $${oldPrice}`);
+        }
+        console.groupEnd();
       }
 
       console.groupEnd();
